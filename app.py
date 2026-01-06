1c012eed0e1b468875746976f54ac63dbc49fa8d
enterreza <101956463+enterreza@users.noreply.github.com>
2026-01-06 16:43:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import urllib.parse
 
 # 1. Konfigurasi Halaman
 st.set_page_config(page_title="Dashboard Marketing 2026", layout="wide")
-st.title("📊 Live Dashboard Timeline Program Kerja")
+st.title("📊 Timeline Dashboard - Program Kerja Marketing helsa Group 2026")
 
 # 2. Identitas Spreadsheet
 SHEET_ID = '17PUXVz1fWFAQlAnNt02BkFPuQFbiBI5uFAOEtZUMluU'
@@ -129,4 +129,5 @@ if df is not None and not df.empty:
         st.warning("Tidak ada data untuk filter yang dipilih.")
 else:
     st.info("💡 Menghubungkan ke Google Sheets...")
+
 
